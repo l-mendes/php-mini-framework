453d7dc3a47eec33ede119cf090739fa7f4896c9
Lucas Mendes Madruga <lucas.madruga@kymo.it>
2024-02-11 20:00:33 -0300

    Initial commit

---
+++ config.example.php
@@ -0,0 +1,12 @@
+<?php 
+
+return [
+    'database' => [
+        'host' => '127.0.0.1',
+        'port' => 3306,
+        'dbname' => 'my_database',
+        'charset' => 'utf8mb4',
+        'username' => 'root',
+        'password' => '',
+    ],
+];
---
+++ controllers/notes/create.php
@@ -0,0 +1,11 @@
+<?php
+
+use Core\App;
+use Core\Database;
+
+$db = App::resolve(Database::class);
+
+view('/notes/create.view.php', [
+    'heading' => 'Create Note',
+    'errors' => []
+]);
